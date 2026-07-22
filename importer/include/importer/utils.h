35f02050b5f77b8cea49ed3fa86041f1aedc4c59
Khalil Henoud <khalilhenoud@gmail.com>
2026-07-22 16:04:49 -0400

    fixes

--- importer/include/importer/utils.h
+++ importer/include/importer/utils.h
@@ -11,137 +11,163 @@
 #pragma once
 
 #include <cassert>
+#include <filesystem>
 #include <vector>
 #include <string>
-#include <filesystem>
-#include <assimp/types.h>
-#include <assimp/material.h>
-#include <math/quatf.h>
-#include <math/vector3f.h>
-#include <entity/mesh/material.h>
-#include <props/color.h>
+// #include <assimp/types.h>
+// #include <assimp/material.h>
+#include <library/containers/cvector.h>
+#include <library/filesystem/io.h>
+#include <library/streams/binary_stream.h>
+// #include <math/quatf.h>
+// #include <math/vector3f.h>
+// #include <entity/mesh/material.h>
+// #include <props/color.h>
 
 
-typedef struct allocator_t allocator_t;
-struct aiScene;
+struct allocator_t;
+// struct aiScene;
+
+// inline
+// void
+// copy_float(float* target, float* source, aiReturn do_copy)
+// {
+//   if (do_copy == AI_SUCCESS)
+//     *target = *source;
+// }
+
+// inline
+// void
+// copy_color(
+//   color_rgba_t* target,
+//   aiColor4D* source,
+//   aiReturn do_copy)
+// {
+//   if (do_copy == AI_SUCCESS) {
+//     target->data[0] = source->r;
+//     target->data[1] = source->g;
+//     target->data[2] = source->b;
+//     target->data[3] = source->a;
+//   }
+// }
+
+// inline
+// void
+// copy_color(
+//   color_rgba_t* target,
+//   aiColor3D* source,
+//   aiReturn do_copy)
+// {
+//   if (do_copy == AI_SUCCESS) {
+//     target->data[0] = source->r;
+//     target->data[1] = source->g;
+//     target->data[2] = source->b;
+//     target->data[3] = 1.f;
+//   }
+// }
+
+// inline
+// void
+// copy_vec3(
+//   vector3f *target,
+//   aiVector3D *source,
+//   aiReturn do_copy)
+// {
+//   if (do_copy == AI_SUCCESS) {
+//     target->data[0] = source->x;
+//     target->data[1] = source->y;
+//     target->data[2] = source->z;
+//   }
+// }
+
+// inline
+// void
+// copy_quat(
+//   quatf *target,
+//   aiQuaternion *source,
+//   aiReturn do_copy)
+// {
+//   if (do_copy == AI_SUCCESS) {
+//     target->data[QUAT_S] = source->w;
+//     target->data[QUAT_X] = source->x;
+//     target->data[QUAT_Y] = source->y;
+//     target->data[QUAT_Z] = source->z;
+//   }
+// }
+
+// inline
+// void
+// copy_texture_transform(
+//   texture_properties_t* target,
+//   aiUVTransform* source,
+//   aiReturn do_copy)
+// {
+//   if (do_copy == AI_SUCCESS) {
+//     target->angle = source->mRotation;
+//     target->u = source->mTranslation.x;
+//     target->v = source->mTranslation.y;
+//     target->u_scale = source->mScaling.x;
+//     target->v_scale = source->mScaling.y;
+//   }
+// }
+
+// inline
+// std::vector<std::filesystem::path>
+// get_all_files_in_directory(std::string directory)
+// {
+//   std::vector<std::filesystem::path> files;
+//   for (const auto& entry : std::filesystem::directory_iterator(directory))
+//     files.push_back(entry.path());
+//   return files;
+// }
+
+// inline
+// void
+// ensure_clean_directory(std::string directory)
+// {
+//   if (std::filesystem::exists(directory)) {
+//     // delete the folder if it already exists.
+//     bool success = std::filesystem::remove_all(directory);
+//     assert(success && "failed to remove the directory and its content");
+//   }
+
+//   // create the folder anew.
+//   bool success = std::filesystem::create_directory(directory);
+//   assert(success && "failed to create the directory");
+// }
 
 inline
 void
-copy_float(float* target, float* source, aiReturn do_copy)
+write_to_file(const binary_stream_t &stream, const std::string &target)
 {
-  if (do_copy == AI_SUCCESS)
-    *target = *source;
+  file_open_flags_t flags =
+  file_open_flags_t(FILE_OPEN_MODE_WRITE | FILE_OPEN_MODE_BINARY);
+  file_handle_t file = open_file(target.c_str(), flags);
+  assert(file);
+  write_buffer(
+    file,
+    stream.data->data, stream.data->elem_data.size, stream.data->size);
+  close_file(file);
 }
 
 inline
 void
-copy_color(
-  color_rgba_t* target,
-  aiColor4D* source,
-  aiReturn do_copy)
+ensure_directory(const std::string &directory)
 {
-  if (do_copy == AI_SUCCESS) {
-    target->data[0] = source->r;
-    target->data[1] = source->g;
-    target->data[2] = source->b;
-    target->data[3] = source->a;
-  }
-}
+  if (std::filesystem::exists(directory))
+    return;
 
-inline
-void
-copy_color(
-  color_rgba_t* target,
-  aiColor3D* source,
-  aiReturn do_copy)
-{
-  if (do_copy == AI_SUCCESS) {
-    target->data[0] = source->r;
-    target->data[1] = source->g;
-    target->data[2] = source->b;
-    target->data[3] = 1.f;
-  }
-}
-
-inline
-void
-copy_vec3(
-  vector3f *target,
-  aiVector3D *source,
-  aiReturn do_copy)
-{
-  if (do_copy == AI_SUCCESS) {
-    target->data[0] = source->x;
-    target->data[1] = source->y;
-    target->data[2] = source->z;
-  }
-}
-
-inline
-void
-copy_quat(
-  quatf *target,
-  aiQuaternion *source,
-  aiReturn do_copy)
-{
-  if (do_copy == AI_SUCCESS) {
-    target->data[QUAT_S] = source->w;
-    target->data[QUAT_X] = source->x;
-    target->data[QUAT_Y] = source->y;
-    target->data[QUAT_Z] = source->z;
-  }
-}
-
-inline
-void
-copy_texture_transform(
-  texture_properties_t* target,
-  aiUVTransform* source,
-  aiReturn do_copy)
-{
-  if (do_copy == AI_SUCCESS) {
-    target->angle = source->mRotation;
-    target->u = source->mTranslation.x;
-    target->v = source->mTranslation.y;
-    target->u_scale = source->mScaling.x;
-    target->v_scale = source->mScaling.y;
-  }
-}
-
-inline
-std::vector<std::filesystem::path>
-get_all_files_in_directory(std::string directory)
-{
-  std::vector<std::filesystem::path> files;
-  for (const auto& entry : std::filesystem::directory_iterator(directory))
-    files.push_back(entry.path());
-  return files;
-}
-
-inline
-void
-ensure_clean_directory(std::string directory)
-{
-  if (std::filesystem::exists(directory)) {
-    // delete the folder if it already exists.
-    bool success = std::filesystem::remove_all(directory);
-    assert(success && "failed to remove the directory and its content");
-  }
-
-  // create the folder anew.
   bool success = std::filesystem::create_directory(directory);
   assert(success && "failed to create the directory");
 }
 
 inline
 std::string
-get_simple_name(std::string path)
+get_simple_name(const std::string &path)
 {
-  path = path.substr(path.find_last_of("/\\") + 1);
-  std::string with_extension = path;
-  std::string extension =
-    with_extension.substr(with_extension.find_last_of("."));
-  return path.substr(0, path.length() - extension.length());
+  std::string file_name = path.substr(path.find_last_of("/\\") + 1);
+  std::string extension = file_name.substr(file_name.find_last_of("."));
+  return file_name.substr(0, file_name.length() - extension.length());
 }
 
 inline
@@ -152,50 +178,50 @@ get_extension(const std::string &path)
   return file_name.substr(file_name.find_last_of(".") + 1);
 }
 
-inline
-std::vector<std::string>
-filter_existing(std::vector<std::string>& files)
-{
-  std::vector<std::string> non_existing;
-  auto iter = files.begin();
-  while (iter != files.end()) {
-    if (!std::filesystem::exists(*iter)) {
-      non_existing.push_back(*iter);
-      iter = files.erase(iter);
-      continue;
-    }
+// inline
+// std::vector<std::string>
+// filter_existing(std::vector<std::string>& files)
+// {
+//   std::vector<std::string> non_existing;
+//   auto iter = files.begin();
+//   while (iter != files.end()) {
+//     if (!std::filesystem::exists(*iter)) {
+//       non_existing.push_back(*iter);
+//       iter = files.erase(iter);
+//       continue;
+//     }
 
-    ++iter;
-  }
+//     ++iter;
+//   }
 
-  return non_existing;
-}
+//   return non_existing;
+// }
 
-inline
-void
-replace_missing_files(
-  std::string target_dir,
-  std::vector<std::string> files,
-  std::string defaulted)
-{
-  for (auto& file : files) {
-    auto target_path = file;
-    target_path = target_path.substr(target_path.find_last_of("/\\") + 1);
-    target_path = target_dir + target_path;
-    bool success = std::filesystem::copy_file(defaulted, target_path);
-    assert(success);
-  }
-}
+// inline
+// void
+// replace_missing_files(
+//   std::string target_dir,
+//   std::vector<std::string> files,
+//   std::string defaulted)
+// {
+//   for (auto& file : files) {
+//     auto target_path = file;
+//     target_path = target_path.substr(target_path.find_last_of("/\\") + 1);
+//     target_path = target_dir + target_path;
+//     bool success = std::filesystem::copy_file(defaulted, target_path);
+//     assert(success);
+//   }
+// }
 
-inline
-void
-copy_files(std::string target_dir, std::vector<std::string> files)
-{
-  for (auto& file : files) {
-    auto target_path = file;
-    target_path = target_path.substr(target_path.find_last_of("/\\") + 1);
-    target_path = target_dir + target_path;
-    bool success = std::filesystem::copy_file(file, target_path);
-    assert(success);
-  }
-}
+// inline
+// void
+// copy_files(std::string target_dir, std::vector<std::string> files)
+// {
+//   for (auto& file : files) {
+//     auto target_path = file;
+//     target_path = target_path.substr(target_path.find_last_of("/\\") + 1);
+//     target_path = target_dir + target_path;
+//     bool success = std::filesystem::copy_file(file, target_path);
+//     assert(success);
+//   }
+// }
--- importer/source/textures.cpp
+++ importer/source/textures.cpp
@@ -8,17 +8,52 @@
  * @copyright Copyright (c) 2026
  *
  */
+#include <cassert>
+#include <cstring>
 #include <importer/textures.h>
+#include <importer/utils.h>
 #include <library/allocator/allocator.h>
+#include <library/filesystem/io.h>
+#include <library/streams/binary_stream.h>
+#include <library/type_registry/type_registry.h>
 #include <loaders/loader_png.h>
+#include <texture/texture_asset.h>
 
 
 uint32_t
 import_texture(
-    const std::string &source_file,
-    const std::string &target_dir,
-    const allocator_t *allocator)
+  const std::string &source_file,
+  const std::string &target_dir,
+  const allocator_t *allocator)
 {
-    loader_png_data_t *data = load_png(source_file.c_str(), allocator);
-    return 1;
+  loader_png_data_t *data = load_png(source_file.c_str(), allocator);
+  assert(data);
+
+  texture_asset_t asset;
+  cvector_setup(
+      &asset.buffer,
+      get_type_data(uint8_t),
+      data->total_buffer_size,
+      allocator);
+  memcpy(asset.buffer.data, data->buffer, data->total_buffer_size);
+  asset.format = (texture_format_t)data->format;
+  asset.width = data->width;
+  asset.height = data->height;
+
+  // serialize to file
+  binary_stream_t stream;
+  binary_stream_def(&stream);
+  binary_stream_setup(&stream, allocator);
+  texture_asset_serialize(&asset, &stream);
+
+  std::string simple_name = get_simple_name(source_file);
+  std::string type_dir = texture_asset_get_dir();
+  std::string target_bin = target_dir + "\\" + type_dir;
+  ensure_directory(target_bin);
+  std::string target_file = target_bin + "\\" + simple_name + ".bin";
+  write_to_file(stream, target_file);
+
+  binary_stream_cleanup(&stream);
+  free_png(data, allocator);
+  return 1;
 }
