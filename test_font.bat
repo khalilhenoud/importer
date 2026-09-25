e5e5dda5263efe7ab52fa6dd2221ae1ae044884a
Khalil Henoud <khalilhenoud@gmail.com>
2026-09-25 10:37:34 -0400

    import font to shared folder test bat

--- test_font.bat
+++ test_font.bat
@@ -1,1 +1,1 @@
-build\Debug\importer "F:\data\font\Malgun Gothic.csv" "F:\data\font\Malgun Gothic.png" F:\data\level1
+build\Debug\importer "F:\data\font\Malgun Gothic.csv" "F:\data\font\Malgun Gothic.png" F:\data\shared
